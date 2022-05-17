983a919f60a6faf4eaf61601ba64ed6ae524f92f
Karol Szcześniak <karolszczesniakk@gmail.com>
2022-05-17 19:49:10 +0200

    XD

--- src/Tests/function.test.ts
+++ src/Tests/function.test.ts
@@ -1,7 +1,7 @@
 import foo from '../function'
 
 describe("function", () => {
-  it("should multiply 5 by 2", () => {
-    expect(foo(5)).toBe(10);
+  it("should multiply 5 by 3", () => {
+    expect(foo(5)).toBe(15);
   })
 })
